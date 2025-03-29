9e4529a377e5c6773cd9d553e0b71d145a5c3b40
morka17 <joshuamorka19@gmail.com>
2025-03-29 17:52:00 +0100

    improve the training loop to make using of the avg loss of the individual multi-GPU  processes

--- train_gpt2.py
+++ train_gpt2.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F 
 import math 
-from torch.distributed import init_process_group
+from torch.distributed import init_process_group, dist
 # ----------------------------------------
 
 class CausalSelfAttention(nn.Module):
@@ -219,9 +219,11 @@ class GPT(nn.Module):
 
 
 class DataLoaderLite: 
-    def __init__(self, B, T):
+    def __init__(self, B, T, process_rank, num_processes ):
         self.B = B 
         self.T = T 
+        self.process_rank = process_rank
+        self.num_processes = num_processes
 
         # at init tokens from disk and store them in memory 
         with open("input.txt", "r") as f: 
@@ -230,10 +232,10 @@ class DataLoaderLite:
         tokens = enc.encode(text)
         self.tokens = torch.tensor(tokens)
         print(f"loaded {len(self.tokens)} tokens")
-        print(f"I epoch = {len(self.tokens) // (B * T)} tokens")
+        # print(f"I epoch = {len(self.tokens) // (B * T)} tokens")
 
         # state 
-        self.current_position = 0 
+        self.current_position = self.B * self.T * self.process_rank 
 
     def next_batch(self):
         B, T = self.B, self.T 
@@ -241,10 +243,10 @@ class DataLoaderLite:
         x = buf[:-1].view(B, T)
         y = buf[1:].view(B, T)
         # advance the position in the tensor 
-        self.current_position += B * T 
+        self.current_position += B * T * self.num_processes
         # if loading the next batch would be out of bounds, reset 
-        if self.current_position + (B * T + 1) > len(self.tokens):
-            self.current_position = 0 
+        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_position = self.B * self.T * self.process_rank 
         return x, y 
 
 
@@ -299,18 +301,18 @@ if master_process:
     print(f"effective batch size: {B * T * grad_accum_steps * ddp_world_size}")
 
     
-print(f"gpu rank: {ddp}")
-print("Bye")
-import sys; sys.exit(0)
 
-train_loader = DataLoaderLite(B=B, T=T)
+train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size)
 # train_loader = DataLoaderLite(B=4, T=32)
 torch.set_float32_matmul_precision('high')
 
-model = GPT(GPTConfig())
-model.eval()
+model = GPT(GPTConfig(vocab_size=50304))
+# model.eval()
 model.to(device)
 mode = torch.compile(model)
+if ddp: 
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[ddp_local_rank])
+    raw_model = model.module if ddp else model 
 
 max_lr = 3e-4 
 min_lr = max_lr * 0.1 
@@ -329,7 +331,8 @@ def get_lr(it):
     return min_lr + coeff * (max_lr - min_lr )
 
 # optimizer = torch.optim.AdamW(model.parameters(), lr=6e-4, betas=(0.9, 0.95), eps=1e-8)
-optimizer = model.configure_optimizers(weight_decay=0.2, learning_rate=6e-4, device=device)
+# optimizer = model.configure_optimizers(weight_decay=0.2, learning_rate=6e-4, device=device)
+optimizer = raw_model.configure_optimizers(weight_decay=0.2, learning_rate=6e-4, device=device)
 
 for i in range(50):
     t0 = time.time()
@@ -343,7 +346,11 @@ for i in range(50):
             # import code; code.interact(local=locals())
         loss = loss / grad_accum_steps
         loss_accum += loss.detach()
+        if ddp: 
+            model.require_backward_grad_sync = (micro_step == grad_accum_steps - 1)
         loss.backward()
+    if ddp: 
+        dist.all_reduce(loss_accum, op=dist.ReduceOp.AVG)
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)  # Prevent the model from gradient shock in case of bad data  batch 
     # determine and set the learning rate for this iteration 
     lr = get_lr(i)
@@ -353,11 +360,13 @@ for i in range(50):
     # torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1 - t0) # time difference in milliseconds 
-    tokens_processed = train_loader.B * train_loader.T * grad_accum_steps
+    tokens_processed = train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size
     tokens_per_sec = tokens_processed / dt 
-    print(f"step {i} | loss: {loss_accum.item():.4f} | lr: {lr:.6e} | norm: {norm:.4f} | dt: {dt:.2f}ms  | tokens/sec: {tokens_per_sec:.2f}")         
+    if master_process:
+        print(f"step {i} |  loss: {loss_accum.item():.4f} | lr: {lr:.6e} | norm: {norm:.4f} | dt: {dt:.2f}ms  | tokens/sec: {tokens_per_sec:.2f}")         
 
-
+if ddp: 
+    dist.destroy_process_group()
 
 # print(loss)
 import sys; sys.exit(0)
